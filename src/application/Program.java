90450090bc79eb9045f88c10bb4a851ab9b65370
Pedro Sartori <pedrosartdr@gmail.com>
2024-07-09 15:45:38 -0300

    Gradiente Descendente - classe

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import entities.Matrix;
 import entities.Metrics;
+import entities.models.GradientDescent;
 import entities.models.OLS_Regression;
 
 
@@ -16,21 +17,25 @@ public class Program
         x = x.sub(x.mean()).div(x.sd());
         y = y.sub(y.mean()).div(y.sd());
 
-        x = Matrix.concat(Matrix.ones(x.shape()[0], 1),x,1);
-
-        Matrix theta = Matrix.random(x.shape()[1], 1);
-
-        for(int i = 0; i < 2000; i++)
-        {
-            Matrix grad = x.T().matmul(x.matmul(theta).sub(y)).mult(2.0/x.shape()[0]);
-            theta = theta.sub(grad.mult(0.01));
-        }
-
-        Matrix y_new = x.matmul(theta);
+//        x = Matrix.concat(Matrix.ones(x.shape()[0], 1),x,1);
+//
+//        Matrix theta = Matrix.random(x.shape()[1], 1);
+//
+//        for(int i = 0; i < 2000; i++)
+//        {
+//            Matrix grad = x.T().matmul(x.matmul(theta).sub(y)).mult(2.0/x.shape()[0]);
+//            theta = theta.sub(grad.mult(0.01));
+//        }
+//
+//        Matrix y_new = x.matmul(theta);
 
 //        OLS_Regression model = new OLS_Regression();
 //        model.fit(x, y);
 //        Matrix y_new = model.predict(x);
+
+        GradientDescent gd = new GradientDescent(10, 0.01);
+        gd.fit(x, y);
+        Matrix y_new = gd.predict(x);
 //
         print(Metrics.r2_score(y, y_new));
     }
---
+++ src/entities/models/GradientDescent.java
@@ -0,0 +1,40 @@
+package entities.models;
+
+import entities.Matrix;
+
+public class GradientDescent
+{
+    // fields
+    private Matrix theta;
+    private int epochs;
+    private double lr;
+
+    // constructors
+    public GradientDescent(int epochs, double lr)
+    {
+        this.epochs = epochs;
+        this.lr = lr;
+    }
+
+    // methods
+    public GradientDescent fit(Matrix x, Matrix y)
+    {
+        x = Matrix.concat(Matrix.ones(x.shape()[0], 1),x,1);
+
+        theta = Matrix.random(x.shape()[1], 1);
+
+        for(int i = 0; i < epochs; i++)
+        {
+            Matrix grad = x.T().matmul(x.matmul(theta).sub(y)).mult(2.0/x.shape()[0]);
+            theta = theta.sub(grad.mult(0.01));
+        }
+
+        return this;
+    }
+
+    public Matrix predict(Matrix x)
+    {
+        Matrix xb = Matrix.concat(Matrix.ones(x.shape()[0],1), x, 1);
+        return xb.matmul(theta);
+    }
+}
